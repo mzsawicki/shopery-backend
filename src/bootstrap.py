ac26877219a4a9956a1f9b34c649dc4b09a064b2
Marcin Sawicki <marcin.sawicki@applover.pl>
2024-05-02 23:47:38 +0200

    Add Redis index creation to bootstrap script

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,5 +1,12 @@
+import asyncio
+
+from redis import Redis
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+
+from src.common.config import Config
 from src.common.s3 import (ObjectStorageGateway, bucket_policy_read_public,
                            get_local_s3_gateway)
+from src.store.model import product as product_schema
 
 S3_BUCKETS = "product-images", "brand-logos"
 
@@ -12,5 +19,20 @@ def bootstrap_s3_buckets(s3: ObjectStorageGateway = get_local_s3_gateway()) -> N
             s3.set_bucket_policy(bucket, bucket_policy_read_public(bucket))
 
 
+def bootstrap_redis_indexes(config: Config = Config()) -> None:
+    client = Redis(host=config.redis_database_host, port=config.redis_database_port)
+    rs = client.ft("idx:products")
+    info = rs.info()
+    if not info:
+        rs.create_index(
+            product_schema,
+            definition=IndexDefinition(
+                prefix=["product:"], index_type=IndexType.JSON
+            )
+        )
+
+
 if __name__ == "__main__":
     bootstrap_s3_buckets()
+    bootstrap_redis_indexes()
+
